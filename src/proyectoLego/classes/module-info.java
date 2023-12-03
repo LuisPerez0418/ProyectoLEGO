12c8a25ac271f92a865092990879371a4f1f0717
LuisPerez0418 <127623670+LuisPerez0418@users.noreply.github.com>
2023-12-03 13:14:38 -0500

    Creacion de Proyecto

---
+++ src/proyectoLego/classes/module-info.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/J2SEModule/module-info.java to edit this template
+ */
+
+module proyectoLego {
+    requires javafx.swt;
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.media;
+    requires javafx.swing;
+    requires javafx.web;
+}
