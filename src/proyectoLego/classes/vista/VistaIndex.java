4dbfd1be7f2016993bac0cc4199dd1b02e00b6d1
LuisPerez0418 <127623670+LuisPerez0418@users.noreply.github.com>
2023-12-03 15:14:40 -0500

    Funcionamiento del VIsta

--- src/proyectoLego/classes/vista/VistaIndex.java
+++ src/proyectoLego/classes/vista/VistaIndex.java
@@ -1,34 +1,23 @@
 package vista;
-
-// @author elias
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
-
 
 public class VistaIndex extends Application {
 
-     @Override
-    public void start(Stage primarystage) throws Exception {
-         try {
-            FXMLLoader loader= new FXMLLoader(getClass()
-                    .getResource("/vista/index.fxml"));
-         Parent parent =loader.load();
-        Scene scene = new Scene(parent);
-        primarystage.initStyle(StageStyle.TRANSPARENT);
-        primarystage.setScene(scene);
-    
-        primarystage.show();
-        } catch (Exception e) {
-             System.out.println(e);
-        }
-    }
-     public static void main(String[] args) {
-        launch(args);
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/vista/index.fxml"));
+        Parent root = loader.load();
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
